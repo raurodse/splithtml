74ed20968df3c90dcdb9e9d15a006b180a4b61ff
Raul Rodrigo Segura <raurodse@gmail.com>
2015-07-31 14:25:45 +0200

    package npm

--- index.js
+++ index.js
@@ -1,190 +1,198 @@
-$(document).ready(function(){
-	$("#documento").load(function(){
-		lista = $("#documento").contents()[0];
-		body = lista.querySelector('body');
-		destino = [];
-		p1 = lista.querySelector('#p1');
-		p2 = lista.querySelector('#p2');
-		p3 = lista.querySelector('#p3');
-		p4 = lista.querySelector('#p4');
-		p5 = lista.querySelector('#p5');
-		p6 = lista.querySelector('#p6');
-		p7 = lista.querySelector('#p7');
-		p8 = lista.querySelector('#p8');
-		p11 = lista.querySelector('#p11');
-		p12 = lista.querySelector('#p12');
-	});
-});
+(function defineHtmlSplitter (global, factory) {
+  if (typeof exports === 'object' && exports && typeof exports.nodeName !== 'string') {
+    factory(exports); // CommonJS
+  } else if (typeof define === 'function' && define.amd) {
+    define(['exports'], factory); // AMD
+  } else {
+    global.HtmlSplitter = {};
+    factory(HtmlSplitter); // script, wsh, asp
+  }
+}(this, function htmlSplitterFactory (htmlsplitter){
 
-function recorre(){
-	agujas = ["p2","p4","p6"];
-	root = "body";
-}
+	/**
+	 * Create a orig content copy from first orig node to needle.
+	 * @param  {Node} orig   Dom node with content to split
+	 * @param  {Node} dest   Destination to result. Usually will be Document Fragment 
+	 * @param  {String} needle End selector.
+	 */
+	function splitTo(orig,dest,needle){
+		var index, node, tempcontainer;
 
+		for(index=0; index < orig.childNodes.length ; index++){
+			node = orig.childNodes[index];
 
-function primercaso(origen,dondeinsertar,aguja){
-	var index;
-	for(index=0; index < origen.childNodes.length ; index++){
-		var node = origen.childNodes[index];
-		if(node.id){
-			var temp = aguja;
-			if(temp.indexOf("#") === 0){
-				temp = temp.substr(1);
-			}
-			if(node.id === temp){
+			if(node.parentNode.querySelector && (node.parentNode.querySelector(needle) === node)){
 				break;
 			}
+
+			if(node.querySelector){
+				if(node.querySelector(needle)){
+					tempcontainer = node.cloneNode();
+					splitTo(node,tempcontainer,needle);
+					dest.appendChild(tempcontainer);
+					break;
+				}
+				else{
+					dest.appendChild(node.cloneNode(true));
+				}
+			}
+			else{
+				dest.appendChild(node.cloneNode(true));
+			}
 		}
-		if(node.querySelector){
-			if(node.querySelector(aguja)){
-				var aux = node.cloneNode();
-				primercaso(node,aux,aguja);
-				dondeinsertar.appendChild(aux);
+	}
+
+	/**
+	 * Search common parent between two nodes from orig
+	 * @param  {node} orig   Dom node with content to search
+	 * @param  {String} firstidentifier First identifier to search parent
+	 * @param  {String} lastidentifier  Second identifier to search parent
+	 * @return {Node}                         Common parent
+	 */
+	function searchCommonParent(orig,firstidentifier,lastidentifier){
+		var commonparent, result, parentstartnode, parentendnode;
+		var startnode = orig.querySelector(firstidentifier);
+		var endnode = orig.querySelector(lastidentifier);
+
+		if(endnode === null){
+			if(orig.parentNode && orig.parentNode.querySelector){
+				if(orig.parentNode.querySelector(lastidentifier) === orig){
+					endnode = orig;
+				}
+			}
+		}
+
+		if(startnode === null){
+			if(orig.parentNode && orig.parentNode.querySelector){
+				if(orig.parentNode.querySelector(firstidentifier) === orig){
+					startnode = orig;
+				}
+			}
+		}
+
+		result = startnode.compareDocumentPosition(endnode);
+		if(result & Node.DOCUMENT_POSITION_CONTAINED_BY){
+			// endnode is contained by startnode
+			return startnode;
+
+		}
+
+		if( result & Node.DOCUMENT_POSITION_CONTAINS){
+			// startnode is contained by endnode
+			return endnode;
+		}
+
+		parentstartnode = startnode.parentNode;
+		parentendnode = endnode.parentNode;
+		while(parentstartnode !== null && parentendnode !== null){
+			if(parentstartnode === parentendnode){
+				// Are siblings
+				commonparent = parentstartnode;
 				break;
 			}
 			else{
-				dondeinsertar.appendChild(node.cloneNode(true));
+				result = parentstartnode.compareDocumentPosition(parentendnode);
+				if(result & Node.DOCUMENT_POSITION_CONTAINED_BY ){
+				// parentstartnode is contained by parentendnode
+					commonparent = parentstartnode;
+					break;
+				}
+
+				if( result & Node.DOCUMENT_POSITION_CONTAINS){
+					// startnode is contained by endnode
+					commonparent = parentendnode;
+					break;
+				}
+			}
+			parentstartnode = parentstartnode.parentNode;
+			parentendnode = parentendnode.parentNode;
+		}
+		return commonparent;
+	}
+
+	/**
+	 * Create a orig content copy from startid node orig node to endid node.
+	 * If endid is same orig then this function return content from startid to end orig
+	 * @param  {node} orig    Dom node with content to split
+	 * @param  {Node} dest    Destination to result. Usually will be Document Fragment 
+	 * @param  {string} startid Start node selector
+	 * @param  {string} endid   End node selector
+	 */
+	function splitFromTo(orig,dest,startid,endid){
+		var commonparent , initialnode, node,
+			container = null,
+			endcase = false,
+			found = false;
+
+		// Same node
+		if(startid === endid){
+			dest.appendChild(orig.querySelector(startid).cloneNode(true))
+			return;
+		}
+		// Not related nodes
+		try{
+			commonparent = searchCommonParent(orig,startid,endid);
+			if(commonparent === orig){
+				endcase = true;
 			}
 		}
-		else{
-			dondeinsertar.appendChild(node.cloneNode(true));
+		catch(err){
+			return;
 		}
-	}
-}
 
-function searchCommonParent(desdedondebuscar,identificadornodoinicio,identificadornodofin){
-	var padrecomun;
-	var nodoinicio = desdedondebuscar.querySelector(identificadornodoinicio);
-	var nodofin = desdedondebuscar.querySelector(identificadornodofin);
+		// Node contain the other node
+		initialnode = orig.querySelector(startid);
+		endnode = orig.querySelector(endid);
+		if(initialnode === commonparent || endnode === commonparent){
+			splitTo(commonparent,dest,endid);
+			return;
+		}
 
-	if(nodofin === null){
-		if(desdedondebuscar.parentNode && desdedondebuscar.parentNode.querySelector){
-			if(desdedondebuscar.parentNode.querySelector(identificadornodofin) === desdedondebuscar){
-				nodofin = desdedondebuscar;
+		node = initialnode;
+
+		loopall:
+		while(true){
+			if(container === null){
+				container = node.parentNode.cloneNode();
+				container.appendChild(node.cloneNode(true));
 			}
-		}
-	}
-
-	if(nodoinicio === null){
-		if(desdedondebuscar.parentNode && desdedondebuscar.parentNode.querySelector){
-			if(desdedondebuscar.parentNode.querySelector(identificadornodoinicio) === desdedondebuscar){
-				nodoinicio = desdedondebuscar;
-			}
-		}
-	}
-
-	var resultado = nodoinicio.compareDocumentPosition(nodofin);
-	var parentinicio, parentfin;
-	if(resultado & Node.DOCUMENT_POSITION_CONTAINED_BY){
-		// Nodofin es contenido por nodoinicio
-		return nodoinicio;
-
-	}
-
-	if( resultado & Node.DOCUMENT_POSITION_CONTAINS){
-		// Nodoinicio es contenido por nodofin
-		
-		return nodofin;
-	}
-
-	parentinicio = nodoinicio.parentNode;
-	parentfin = nodofin.parentNode;
-	while(parentinicio !== null && parentfin !== null){
-		if(parentinicio === parentfin){
-			// Son hermanos directos
-			padrecomun = parentinicio;
-			break;
-		}
-		else{
-			resultado = parentinicio.compareDocumentPosition(parentfin);
-			if(resultado & Node.DOCUMENT_POSITION_CONTAINED_BY ){
-			// parentinicio es contenido por parentfin
-				padrecomun = parentinicio;
-				break;
+			else{
+				var aux = container;
+				container = node.parentNode.cloneNode();
+				container.appendChild(aux);
 			}
 
-			if( resultado & Node.DOCUMENT_POSITION_CONTAINS){
-				// Nodoinicio es contenido por nodofin
-				padrecomun = parentfin;
-				break;
-			}
-		}
-		parentinicio = parentinicio.parentNode;
-		parentfin = parentfin.parentNode;
-	}
-	return padrecomun;
-}
+			loopsamelevel:
+			while(node.nextSibling !== null){
+				node = node.nextSibling;
 
-function complexCase(orig,dest,startid,endid){
-	var commonparent , initialnode, node,
-		container = null,
-		endcase = false,
-		found = false;
-
-	debugger;
-	// Same node
-	if(startid === endid){
-		dest.appendChild(orig.querySelector(startid).cloneNode(true))
-		return;
-	}
-	// Not related nodes
-	try{
-		commonparent = searchCommonParent(orig,startid,endid);
-		if(commonparent === orig){
-			endcase = true;
-		}
-	}
-	catch(err){
-		return;
-	}
-
-	// Node contain the other node
-	initialnode = orig.querySelector(startid);
-	endnode = orig.querySelector(endid);
-	if(initialnode === commonparent || endnode === commonparent){
-		primercaso(commonparent,dest,endid);
-		return;
-	}
-
-	node = initialnode;
-
-	loopall:
-	while(true){
-		if(container === null){
-			container = node.parentNode.cloneNode();
-			container.appendChild(node.cloneNode(true));
-		}
-		else{
-			var aux = container;
-			container = node.parentNode.cloneNode();
-			container.appendChild(aux);
-		}
-
-		loopsamelevel:
-		while(node.nextSibling !== null){
-			node = node.nextSibling;
-
-			if(node.parentNode.querySelector && (node.parentNode.querySelector(endid) === node)){
-				break loopall;
-			}
-
-			if(node.querySelector && node.querySelector(endid)){
-					var aux = node.cloneNode();
-					primercaso(node,aux,endid);
-					container.appendChild(aux);
+				if(node.parentNode.querySelector && (node.parentNode.querySelector(endid) === node)){
 					break loopall;
+				}
+
+				if(node.querySelector && node.querySelector(endid)){
+						var aux = node.cloneNode();
+						splitTo(node,aux,endid);
+						container.appendChild(aux);
+						break loopall;
+				}
+				container.appendChild(node.cloneNode(true));
 			}
-			container.appendChild(node.cloneNode(true));
+			node = node.parentNode;
+			if(endcase){
+				if(node.parentNode && node.parentNode.querySelector && (node.parentNode.querySelector(endid) === node)){
+					break loopall;
+				}
+			}
 		}
-		node = node.parentNode;
-		if(endcase){
-			if(node.parentNode && node.parentNode.querySelector && (node.parentNode.querySelector(endid) === node)){
-				break loopall;
-			}
+			
+		for( i = 0; i < container.childNodes.length; i++){
+			dest.appendChild(container.childNodes[i].cloneNode(true));
 		}
 	}
-		
-	for( i = 0; i < container.childNodes.length; i++){
-		dest.appendChild(container.childNodes[i].cloneNode(true));
-	}
+
+	htmlsplitter.splitFromTo = splitFromTo;
+	htmlsplitter.splitTo = splitTo;
+	htmlsplitter.searchCommonParent = searchCommonParent;
 }
+));
